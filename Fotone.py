54a0cd9e21c769463e0bd6f2b97f142d267dfa80
francescovetrulli <francesco.vetrulli@gmail.com>
2026-01-06 15:54:14 +0100

    File contenenti le classi fotone e particella

---
+++ Fotone.py
@@ -0,0 +1,35 @@
+class Fotone:
+	
+	def __init__(self, energia):
+		'''
+		Crea un fotone.
+
+		Parametri:
+			energia(float) : Energia del fotone
+		Returns:
+			None
+		'''
+		
+		self.energia = energia
+		 
+	def coppie(self, sciame, dE, s):
+		'''
+		Simulo la produzione di coppie elettrone/positrone da parte 
+		del fotone.
+
+		Parametri:
+			sciame(list): Particelle presenti nello sciame
+			dE(float): Perdita per ionizzazione in una lunghezza di 
+			radiazione [Mev]
+			s(float): Passo della simulazione
+		Returns:
+			None (aggiorna la lista delle particelle presenti, 
+			aggiungendo una coppia elettrone positrone 
+			con metà dell'energia iniziale)
+		'''
+		
+		from Particella import Particella
+		nuova_energia = self.energia / 2
+		sciame.append(Particella(nuova_energia, dE, s, +1))
+		sciame.append(Particella(nuova_energia, dE, s, -1))
+		
---
+++ Particella.py
@@ -0,0 +1,52 @@
+class Particella:
+
+	def __init__(self, energia, dE, s, segno):
+		'''
+		Crea una particella.
+
+		Parametri:
+			energia(float) : Energia della particella
+			dE(float): Perdita per ionizzazione in una lunghezza di 
+			radiazione [Mev]
+			s(float): Passo della simulazione in frazioni di lunghezza 
+			di radiazione
+			segno(int) : +1 per positroni, -1 per elettroni
+		Attributi:
+			energia_soglia(float): Energia minima per ionizzare, 
+			è l'energia persa per ionizzazione in uno step [MeV]
+		'''
+		self.energia = energia
+		self.energia_soglia = dE * s
+		self.segno = segno
+
+	def emissione(self, sciame):
+		'''
+		Simula il Bremsstrahlung dell'elettrone.
+
+		Parametri:
+			sciame(list): Particelle  presenti nello sciame
+		Returns:
+			Fotone(Fotone): Fotone prodotto con metà energia. 
+		'''
+		
+		from Fotone import Fotone
+		nuova_energia = self.energia / 2
+		self.energia = nuova_energia
+		sciame.append(Fotone(nuova_energia))
+		
+	def ionizzazione(self):
+		'''
+		Simula la perdita di energia per ionizzazione.
+
+		Parametri:
+			
+		Returns:
+			None (modifica l'energia della particella sottraendo 
+			l'energia di soglia)
+		'''
+		self.energia -= self.energia_soglia
+
+
+
+
+    
